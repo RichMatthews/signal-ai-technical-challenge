b9fa487aed51d0e5b636d5a920ea0c1c239d06cd
Richard Matthews <richardmatthews@wb1008-richardmatthews.local>
2022-01-17 13:57:03 +0000

    Add correct styles for show imags

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,47 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
+import { useCallback, useEffect, useState } from "react";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
-  return <div className={styles.container}>Home Page</div>;
+  const [search, setSearch] = useState("");
+  const [tvShows, setTvShows] = useState([]);
+
+  const searchForShows = useCallback(() => {
+    console.log(search, "the search?");
+    fetch(`https://api.tvmaze.com/search/shows?q=${search}`)
+      .then((res) => res.json())
+      .then((r) => setTvShows(r));
+  }, [search]);
+
+  useEffect(() => {
+    searchForShows();
+  }, [searchForShows]);
+
+  const searchHandler = (e) => {
+    setSearch(e.target.value);
+  };
+  return (
+    <div className={styles.container}>
+      <div className={styles.contentContainer}>
+        <input
+          placeholder="Search for a tvshow"
+          onChange={searchHandler}
+          className={styles.input}
+        />
+        <div className={styles.showsContainer}>
+          {tvShows?.map((show) => (
+            <div className={styles.show}>
+              <img
+                src={show?.show?.image?.original}
+                className={styles.showImage}
+              />
+              <h3>{show.show.name}</h3>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
