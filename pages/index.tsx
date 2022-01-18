3973c94a9adee13608f661667ae474e1ebd88916
Richard Matthews <richardmatthews@wb1008-richardmatthews.local>
2022-01-18 12:08:33 +0000

    Add axios to use for multiple concurrent requests

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,26 @@
 import type { NextPage } from "next";
+import Image from "next/image";
+import Link from "next/link";
 import { useCallback, useEffect, useState } from "react";
 import styles from "../styles/Home.module.css";
+
+type Show = {
+  id: string;
+  name: string;
+  genres: string[];
+  image: {
+    medium: string;
+  };
+};
 
 const Home: NextPage = () => {
   const [search, setSearch] = useState("");
   const [tvShows, setTvShows] = useState([]);
 
   const searchForShows = useCallback(() => {
-    console.log(search, "the search?");
     fetch(`https://api.tvmaze.com/search/shows?q=${search}`)
       .then((res) => res.json())
-      .then((r) => setTvShows(r));
+      .then((res) => setTvShows(res));
   }, [search]);
 
   useEffect(() => {
@@ -20,6 +30,26 @@ const Home: NextPage = () => {
   const searchHandler = (e) => {
     setSearch(e.target.value);
   };
+
+  const renderPriorityImage = (medium) => {
+    return (
+      <img
+        alt="needsChanging"
+        src={medium ? medium : "/images/not-available.png"}
+        className={styles.showImage}
+      />
+    );
+    // return (
+    //   <Image
+    //     alt="needsChanging"
+    //     src={original ? original : medium ? medium : ""}
+    //     // className={styles.showImage}
+    //     layout="fill"
+    //     objectFit=""
+    //   />
+    // );
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.contentContainer}>
@@ -29,14 +59,19 @@ const Home: NextPage = () => {
           className={styles.input}
         />
         <div className={styles.showsContainer}>
-          {tvShows?.map((show) => (
-            <div className={styles.show}>
-              <img
-                src={show?.show?.image?.original}
-                className={styles.showImage}
-              />
-              <h3>{show.show.name}</h3>
-            </div>
+          {tvShows?.map(({ show }: Show) => (
+            <Link href={`/shows/${show.id}`}>
+              <div className={styles.show} key={show?.id}>
+                {console.log(show)}
+                <div className={styles.imageContainer}>
+                  {renderPriorityImage(show?.image?.medium)}
+                </div>
+                <h4 className={styles.heading4}>{show?.name}</h4>
+                <p className={styles.subHeading}>
+                  {show?.genres?.slice(0, 2).join(", ")}
+                </p>
+              </div>
+            </Link>
           ))}
         </div>
       </div>
--- pages/shows/[id].tsx
+++ pages/shows/[id].tsx
@@ -1,9 +1,87 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
+import { useCallback, useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import axios from "axios";
+
+const regex = /(<([^>]+)>)/gi;
 
 const Show: NextPage = () => {
-  return <div>Specific Show Page</div>;
+  const router = useRouter();
+  const { id } = router.query;
+  const [show, setShow] = useState();
+
+  const getEmbeddedShowDetails = useCallback(async () => {
+    axios
+      .all([
+        axios.get(`https://api.tvmaze.com/shows/${id}`),
+        axios.get(`https://api.tvmaze.com/shows/${id}/cast`),
+        axios.get(`https://api.tvmaze.com/shows/${id}/seasons`),
+      ])
+      .then(
+        axios.spread(({ data }, { data: cast }, { data: seasons }) => {
+          const amalgamatedData = {
+            ...data,
+            cast,
+            seasons,
+          };
+          setShow(amalgamatedData);
+        })
+      );
+  }, [id]);
+
+  useEffect(() => {
+    if (id) {
+      //   fetch(`https://api.tvmaze.com/shows/${id}`)
+      //     .then((res) => res.json())
+      //     .then((res) => setShow(res));
+      getEmbeddedShowDetails();
+    }
+  }, [getEmbeddedShowDetails, id]);
+
+  console.log(show?.seasons);
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        width: "500px",
+        flexDirection: "column",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "flex-end",
+          justifyContent: "space-between",
+          background: "red",
+          width: "100%",
+        }}
+      >
+        <img src={show?.image?.medium} style={{ marginRight: "10px" }} />
+        <h2 style={{ margin: 0 }}>{show?.name}</h2>
+        <div>Favourite goes here</div>
+      </div>
+      <div>{show?.summary.replace(regex, "")}</div>
+
+      <h3>Cast</h3>
+      <div>
+        {show?.cast.map(({ person }) => (
+          <div>{person?.name}</div>
+        ))}
+      </div>
+
+      <div>
+        {show?.seasons.map((season) => (
+          <>
+            {console.log(season)}
+            <div>{season?.number}</div>
+          </>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Show;
